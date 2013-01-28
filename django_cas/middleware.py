c3f439e515f40a717bd97d94a640698964c8c138
gstrat <gstrat@yawd.eu>
2013-01-28 02:19:33 +0200

    move CAS login middleware in django-cas2

--- django_cas/middleware.py
+++ django_cas/middleware.py
@@ -4,10 +4,14 @@ from django.conf import settings
 from django.contrib import auth
 from django.contrib.auth.views import login, logout
 from django.core.exceptions import PermissionDenied
+from django.core.urlresolvers import resolve
 from django.http import HttpResponseRedirect
+from django.utils.http import urlquote_plus
 from django_cas.exceptions import CasTicketException
 from django_cas.views import login as cas_login, logout as cas_logout
 from urllib import urlencode
+import base64
+import hashlib
 
 __all__ = ['CASMiddleware']
 
@@ -36,7 +40,7 @@ class CASMiddleware(object):
         
         # The rest of this method amends the Django admin authorization wich
         # will post a username/password dialog to authenticate to django admin.
-        if not view_func.__module__.startswith('django.contrib.admin.'):
+        if not view_func.__module__.startswith('yawdadmin.'):
             return None
 
         if request.user.is_authenticated():
@@ -56,3 +60,33 @@ class CASMiddleware(object):
             return HttpResponseRedirect(request.path)
         else:
             return None
+
+class CASLoginMiddleware(object):
+    def process_request(self, request):
+        if request.method == 'GET' and not request.user.is_authenticated() and 'elorus_cas' in request.COOKIES and request.COOKIES['elorus_cas'] == self.hash_value('login'):
+            current_view = resolve(request.path)[0]
+            if not hasattr(current_view, 'func_name') or current_view.func_name not in  ('login', 'logout'):
+                return HttpResponseRedirect('%s?next=%s' % (settings.LOGIN_URL, urlquote_plus(request.get_full_path())))
+        elif request.method == 'GET' and request.user.is_authenticated() and 'elorus_cas' in request.COOKIES and request.COOKIES['elorus_cas'] == self.hash_value('logout'):
+            current_view = resolve(request.path)[0]
+            if not hasattr(current_view, 'func_name') or current_view.func_name not in  ('login', 'logout'):
+                return HttpResponseRedirect('%s?next=%s' % (settings.LOGOUT_URL, urlquote_plus(request.get_full_path())))
+
+    def process_response(self, request, response):
+        if request.user.is_authenticated() and not 'elorus_cas' in request.COOKIES:
+            #by default the cookie expires when the session is over.. so.. perfect!
+            response.set_cookie(key='elorus_cas', value=self.hash_value('login'), domain=settings.ELEP_DOMAIN, max_age=request.session.get_expiry_age(), httponly=True)
+        elif not request.user.is_authenticated() and 'elorus_cas' in request.COOKIES and request.COOKIES['elorus_cas'] == self.hash_value('login'):
+            current_view = resolve(request.path)[0]
+            if hasattr(current_view, 'func_name') and current_view.func_name == 'logout':
+                response.set_cookie(key='elorus_cas', value=self.hash_value('logout'), domain=settings.ELEP_DOMAIN)
+        elif request.user.is_authenticated() and 'elorus_cas' in request.COOKIES and request.COOKIES['elorus_cas'] == self.hash_value('logout'):
+            current_view = resolve(request.path)[0]
+            if hasattr(current_view, 'func_name') and current_view.func_name == 'login':
+                response.set_cookie(key='elorus_cas', value=self.hash_value('logout'), domain=settings.ELEP_DOMAIN, max_age=request.session.get_expiry_age())
+        return response
+    
+    def hash_value(self, value):
+        m = hashlib.md5()
+        m.update(value)
+        return base64.b64encode(m.digest())
