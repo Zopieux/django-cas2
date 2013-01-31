35cfcd4af10f2cdd5e0bda0a1db6c4264490801f
ppetrid <ppetrid@yawd.eu>
2013-01-31 20:08:43 +0200

    fix bug with unresolved urls

--- django_cas/middleware.py
+++ django_cas/middleware.py
@@ -13,7 +13,9 @@ from urllib import urlencode
 import base64
 import hashlib
 
+
 __all__ = ['CASMiddleware']
+
 
 class CASMiddleware(object):
     """Middleware that allows CAS authentication on admin pages"""
@@ -26,7 +28,6 @@ class CASMiddleware(object):
                  "setting to insert 'django.contrib.auth.middleware."
                  "AuthenticationMiddleware'.")
         assert hasattr(request, 'user'), error
-
 
     def process_view(self, request, view_func, view_args, view_kwargs):
         """ Forwards unauthenticated requests to the admin page to the CAS
@@ -51,7 +52,6 @@ class CASMiddleware(object):
         params = urlencode({auth.REDIRECT_FIELD_NAME: request.get_full_path()})        
         return HttpResponseRedirect(settings.LOGIN_URL + '?' + params)
 
-
     def process_exception(self, request, exception):
         """ When we get a CasTicketException it is probably caused by the ticket timing out.
             So logout and get the same page again."""
@@ -60,6 +60,7 @@ class CASMiddleware(object):
             return HttpResponseRedirect(request.path)
         else:
             return None
+
 
 class CASLoginMiddleware(object):
     def process_request(self, request):
@@ -77,15 +78,23 @@ class CASLoginMiddleware(object):
             #by default the cookie expires when the session is over.. so.. perfect!
             response.set_cookie(key='elorus_cas', value=self.hash_value('login'), domain=settings.ELEP_DOMAIN, max_age=request.session.get_expiry_age(), httponly=True)
         elif (not hasattr(request, 'user') or not request.user.is_authenticated()) and 'elorus_cas' in request.COOKIES and request.COOKIES['elorus_cas'] == self.hash_value('login'):
-            current_view = resolve(request.path)[0]
-            if hasattr(current_view, 'func_name') and current_view.func_name == 'logout':
-                response.set_cookie(key='elorus_cas', value=self.hash_value('logout'), domain=settings.ELEP_DOMAIN)
+            try:
+                current_view = resolve(request.path)[0]
+            except:
+                pass
+            else:
+                if hasattr(current_view, 'func_name') and current_view.func_name == 'logout':
+                    response.set_cookie(key='elorus_cas', value=self.hash_value('logout'), domain=settings.ELEP_DOMAIN)
         elif hasattr(request, 'user') and request.user.is_authenticated() and 'elorus_cas' in request.COOKIES and request.COOKIES['elorus_cas'] == self.hash_value('logout'):
-            current_view = resolve(request.path)[0]
-            if hasattr(current_view, 'func_name') and current_view.func_name == 'login':
-                response.set_cookie(key='elorus_cas', value=self.hash_value('login'), domain=settings.ELEP_DOMAIN, max_age=request.session.get_expiry_age())
+            try:
+                current_view = resolve(request.path)[0]
+            except:
+                pass
+            else:
+                if hasattr(current_view, 'func_name') and current_view.func_name == 'login':
+                    response.set_cookie(key='elorus_cas', value=self.hash_value('login'), domain=settings.ELEP_DOMAIN, max_age=request.session.get_expiry_age())
         return response
-    
+
     def hash_value(self, value):
         m = hashlib.md5()
         m.update(value)
